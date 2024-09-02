baaa20d839da47dd314b56dc91d752ec50a3ae53
CoolerProYT <veronlam1818@gmail.com>
2024-09-02 13:38:17 +0800

    changed egg color

--- src/main/java/com/coolerpromc/productiveslimes/item/ModItems.java
+++ src/main/java/com/coolerpromc/productiveslimes/item/ModItems.java
@@ -24,21 +24,21 @@ public class ModItems {
     public static final DeferredItem<Item> DIRT_SLIME_SPAWN_EGG = ITEMS.register("dirt_slime_spawn_egg",
             () -> new SpawnEggItem(ModEntities.DIRT_SLIME.get(), 0x5e3a1c, 0x885022, new Item.Properties()));
     public static final DeferredItem<Item> STONE_SLIME_SPAWN_EGG = ITEMS.register("stone_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.STONE_SLIME.get(), 0x5e3a1c, 0x885022, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.STONE_SLIME.get(), 0x4a4545, 0x918b8b, new Item.Properties()));
     public static final DeferredItem<Item> IRON_SLIME_SPAWN_EGG = ITEMS.register("iron_slime_spawn_egg",
             () -> new SpawnEggItem(ModEntities.IRON_SLIME.get(), 0x898c8a, 0xcfd1d0, new Item.Properties()));
     public static final DeferredItem<Item> COPPER_SLIME_SPAWN_EGG = ITEMS.register("copper_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.COPPER_SLIME.get(), 0x898c8a, 0xcfd1d0, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.COPPER_SLIME.get(), 0x6a3e15, 0xb87333, new Item.Properties()));
     public static final DeferredItem<Item> GOLD_SLIME_SPAWN_EGG = ITEMS.register("gold_slime_spawn_egg",
             () -> new SpawnEggItem(ModEntities.GOLD_SLIME.get(), 0xa5953f, 0xd9c245, new Item.Properties()));
     public static final DeferredItem<Item> DIAMOND_SLIME_SPAWN_EGG = ITEMS.register("diamond_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.DIAMOND_SLIME.get(), 0x898c8a, 0xcfd1d0, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.DIAMOND_SLIME.get(), 0x178f9c, 0x2fcadc, new Item.Properties()));
     public static final DeferredItem<Item> NETHERITE_SLIME_SPAWN_EGG = ITEMS.register("netherite_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.NETHERITE_SLIME.get(), 0x898c8a, 0xcfd1d0, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.NETHERITE_SLIME.get(), 0x4c2b2b, 0x453a3a, new Item.Properties()));
     public static final DeferredItem<Item> LAPIS_SLIME_SPAWN_EGG = ITEMS.register("lapis_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.LAPIS_SLIME.get(), 0x898c8a, 0xcfd1d0, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.LAPIS_SLIME.get(), 0x1c41ba, 0x2853e0, new Item.Properties()));
     public static final DeferredItem<Item> REDSTONE_SLIME_SPAWN_EGG = ITEMS.register("redstone_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.REDSTONE_SLIME.get(), 0x898c8a, 0xcfd1d0, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.REDSTONE_SLIME.get(), 0xa10505, 0xc92020, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
