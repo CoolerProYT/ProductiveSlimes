c4a1fa2a057c0dbf2ef2c5ec683779709e43d468
CoolerProYT <veronlam1818@gmail.com>
2024-09-19 18:06:28 +0800

    slimeball and slime block texture now hard coded

--- src/main/java/com/coolerpromc/productiveslimes/datagen/ModItemModelProvider.java
+++ src/main/java/com/coolerpromc/productiveslimes/datagen/ModItemModelProvider.java
@@ -3,6 +3,7 @@ package com.coolerpromc.productiveslimes.datagen;
 import com.coolerpromc.productiveslimes.ProductiveSlimes;
 import com.coolerpromc.productiveslimes.fluid.ModFluids;
 import com.coolerpromc.productiveslimes.item.ModItems;
+import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -21,15 +22,15 @@ public class ModItemModelProvider extends ItemModelProvider {
     protected void registerModels() {
         simpleItem(ModItems.GUIDEBOOK);
 
-        simpleItem(ModItems.DIRT_SLIME_BALL);
-        simpleItem(ModItems.STONE_SLIME_BALL);
-        simpleItem(ModItems.IRON_SLIME_BALL);
-        simpleItem(ModItems.COPPER_SLIME_BALL);
-        simpleItem(ModItems.GOLD_SLIME_BALL);
-        simpleItem(ModItems.DIAMOND_SLIME_BALL);
-        simpleItem(ModItems.NETHERITE_SLIME_BALL);
-        simpleItem(ModItems.LAPIS_SLIME_BALL);
-        simpleItem(ModItems.REDSTONE_SLIME_BALL);
+        slimeballItem(ModItems.DIRT_SLIME_BALL);
+        slimeballItem(ModItems.STONE_SLIME_BALL);
+        slimeballItem(ModItems.IRON_SLIME_BALL);
+        slimeballItem(ModItems.COPPER_SLIME_BALL);
+        slimeballItem(ModItems.GOLD_SLIME_BALL);
+        slimeballItem(ModItems.DIAMOND_SLIME_BALL);
+        slimeballItem(ModItems.NETHERITE_SLIME_BALL);
+        slimeballItem(ModItems.LAPIS_SLIME_BALL);
+        slimeballItem(ModItems.REDSTONE_SLIME_BALL);
 
         basicItem(ModFluids.MOLTEN_DIRT_BUCKET.get());
         basicItem(ModFluids.MOLTEN_STONE_BUCKET.get());
@@ -50,6 +51,20 @@ public class ModItemModelProvider extends ItemModelProvider {
         withExistingParent(ModItems.NETHERITE_SLIME_SPAWN_EGG.getId().getPath(), mcLoc("item/template_spawn_egg"));
         withExistingParent(ModItems.LAPIS_SLIME_SPAWN_EGG.getId().getPath(), mcLoc("item/template_spawn_egg"));
         withExistingParent(ModItems.REDSTONE_SLIME_SPAWN_EGG.getId().getPath(), mcLoc("item/template_spawn_egg"));
+    }
+
+    private ItemModelBuilder slimeballItem(DeferredItem<Item> item){
+        return getBuilder(item.getId().getPath())
+                .parent(getExistingFile(mcLoc("item/generated")))
+                .texture("layer0", ResourceLocation.fromNamespaceAndPath(ProductiveSlimes.MODID, "item/template_slimeball"))
+                .element()
+                .face(Direction.DOWN).texture("#layer0").tintindex(0).end()
+                .face(Direction.UP).texture("#layer0").tintindex(0).end()
+                .face(Direction.NORTH).texture("#layer0").tintindex(0).end()
+                .face(Direction.SOUTH).texture("#layer0").tintindex(0).end()
+                .face(Direction.WEST).texture("#layer0").tintindex(0).end()
+                .face(Direction.EAST).texture("#layer0").tintindex(0).end()
+                .end();
     }
 
     private ItemModelBuilder simpleItem(DeferredItem<Item> item){
