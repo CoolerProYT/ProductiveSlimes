8048df511e09174e13477eef15295115fd01caa4
Chesy <chestedgt@gmail.com>
2024-09-20 10:26:55 +0800

    changed oak egg color

--- src/main/java/com/coolerpromc/productiveslimes/item/ModItems.java
+++ src/main/java/com/coolerpromc/productiveslimes/item/ModItems.java
@@ -46,7 +46,7 @@ public class ModItems {
     public static final DeferredItem<Item> REDSTONE_SLIME_SPAWN_EGG = ITEMS.register("redstone_slime_spawn_egg",
             () -> new SpawnEggItem(ModEntities.REDSTONE_SLIME.get(), 0xa10505, 0xc92020, new Item.Properties()));
     public static final DeferredItem<Item> OAK_SLIME_SPAWN_EGG = ITEMS.register("oak_slime_spawn_egg",
-            () -> new SpawnEggItem(ModEntities.OAK_SLIME.get(), 0xa69d6f, 0x453a3a, new Item.Properties()));
+            () -> new SpawnEggItem(ModEntities.OAK_SLIME.get(), 0xa69d6f, 0xded4a4, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
