449e76c6c4398a18ffb85ada860ef4b0f6b401a1
CoolerProYT <j22036431@student.newinti.edu.my>
2024-10-31 14:25:36 +0800

    fixed hitbox

--- src/main/java/com/coolerpromc/productiveslimes/entity/slime/BaseSlime.java
+++ src/main/java/com/coolerpromc/productiveslimes/entity/slime/BaseSlime.java
@@ -10,7 +10,9 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.MoveControl;
@@ -22,6 +24,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumSet;
@@ -118,6 +121,25 @@ public abstract class BaseSlime extends Slime {
         pBuilder.define(GROWTH_COUNTER, 0);
     }
 
+    @Override
+    protected Vec3 getPassengerAttachmentPoint(Entity entity, EntityDimensions dimensions, float partialTick) {
+        return new Vec3(0.0, (double)dimensions.height() - 0.015625 * (double)this.getSize() * (double)partialTick, 0.0);
+    }
+
+    @Override
+    public void onSyncedDataUpdated(EntityDataAccessor<?> key) {
+        if (ID_SIZE.equals(key)) {
+            this.refreshDimensions();
+            this.setYRot(this.yHeadRot);
+            this.yBodyRot = this.yHeadRot;
+            if (this.isInWater() && this.random.nextInt(20) == 0) {
+                this.doWaterSplashEffect();
+            }
+        }
+
+        super.onSyncedDataUpdated(key);
+    }
+
     public abstract void dropResource();
 
     @Override
@@ -198,26 +220,17 @@ public abstract class BaseSlime extends Slime {
 
     @Override
     public void refreshDimensions() {
-        double width = 0.6F * (float)this.getSize();
-        double height = 0.8F * (float)this.getSize();
-        this.setBoundingBox(new AABB(-width / 2.0D, 0.0D, -width / 2.0D, width / 2.0D, height, width / 2.0D));
+        double d0 = this.getX();
+        double d1 = this.getY();
+        double d2 = this.getZ();
         super.refreshDimensions();
+        this.setBoundingBox(new AABB(this.getX(), this.getY(), this.getZ(), this.getX() + (double)this.getBbWidth(), this.getY() + (double)this.getBbHeight(), this.getZ() + (double)this.getBbWidth()));
+        this.setPos(d0, d1, d2);
     }
 
-    public void transformSlime(Player pPlayer, InteractionHand pHand, BaseSlime originalSlime, BaseSlime newSlime){
-        ItemStack itemStack = pPlayer.getItemInHand(pHand);
-
-        if (!pPlayer.getAbilities().instabuild){
-            itemStack.shrink(originalSlime.getSize() + 1);
-        }
-
-        if (newSlime != null) {
-            newSlime.moveTo(originalSlime.getX(), originalSlime.getY(), originalSlime.getZ(), originalSlime.getYRot(), originalSlime.getXRot());
-            newSlime.setSize(originalSlime.getSize(), true);
-            this.level().addFreshEntity(newSlime);
-        }
-
-        originalSlime.discard();
+    @Override
+    public EntityDimensions getDefaultDimensions(Pose pose) {
+        return EntityDimensions.scalable((float) (0.5 * (float)this.getSize()), (float) (0.5 * (float)this.getSize()));
     }
 
     public void growthSlime(Player pPlayer, InteractionHand pHand, BaseSlime slime){
