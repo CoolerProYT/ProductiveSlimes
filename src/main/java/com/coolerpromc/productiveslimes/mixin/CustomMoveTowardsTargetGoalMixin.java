43c09fa8e7d7c7a268f949cca5667154997ec9e4
CoolerProYT <veronlam1818@gmail.com>
2024-11-29 23:03:24 +0800

    mixin

---
+++ src/main/java/com/coolerpromc/productiveslimes/mixin/CustomMoveTowardsTargetGoalMixin.java
@@ -0,0 +1,43 @@
+package com.coolerpromc.productiveslimes.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.MoveTowardsTargetGoal;
+import net.minecraft.world.entity.monster.Slime;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MoveTowardsTargetGoal.class)
+public abstract class CustomMoveTowardsTargetGoalMixin {
+    @Shadow public abstract void stop();
+
+    @Accessor
+    public abstract PathfinderMob getMob();
+
+    @Inject(method = "start", at = @At("HEAD"), cancellable = true)
+    private void start(CallbackInfo ci) {
+        if (getMob().getTarget() instanceof Slime) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "canUse", at = @At("HEAD"), cancellable = true)
+    private void canUse(CallbackInfoReturnable<Boolean> cir) {
+        if (getMob().getTarget() instanceof Slime) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "canContinueToUse", at = @At("HEAD"), cancellable = true)
+    private void canContinueToUse(CallbackInfoReturnable<Boolean> cir) {
+        if (getMob().getTarget() instanceof Slime) {
+            cir.setReturnValue(false);
+        }
+    }
+}
