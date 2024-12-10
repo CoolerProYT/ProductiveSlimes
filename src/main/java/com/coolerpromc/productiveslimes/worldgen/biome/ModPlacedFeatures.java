45c25ae57387b7bfe3c56ec4539778cc87314a29
CoolerProYT <veronlam1818@gmail.com>
2024-12-10 20:20:48 +0800

    Added back lake generation in slime land

--- src/main/java/com/coolerpromc/productiveslimes/worldgen/biome/ModPlacedFeatures.java
+++ src/main/java/com/coolerpromc/productiveslimes/worldgen/biome/ModPlacedFeatures.java
@@ -17,25 +17,10 @@ import net.minecraft.world.level.levelgen.placement.*;
 import java.util.List;
 
 public class ModPlacedFeatures {
-    public static ResourceKey<PlacedFeature> LAKE_MOLTEN_DIRT = registerKey("lake_molten_dirt");
-    public static ResourceKey<PlacedFeature> LAKE_MOLTEN_STONE = registerKey("lake_molten_stone");
-
     public static ResourceKey<PlacedFeature> SLIMY_TREE = registerKey("slimy_tree");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context){
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatureGetter = context.lookup(Registries.CONFIGURED_FEATURE);
-
-        register(context, ModPlacedFeatures.LAKE_MOLTEN_DIRT, configuredFeatureGetter.getOrThrow(ModConfiguredFeatures.LAKE_MOLTEN_DIRT), List.of(
-                RarityFilter.onAverageOnceEvery(20),
-                InSquarePlacement.spread(),
-                HeightmapPlacement.onHeightmap(Heightmap.Types.WORLD_SURFACE_WG)
-        ));
-        register(context, ModPlacedFeatures.LAKE_MOLTEN_STONE, configuredFeatureGetter.getOrThrow(ModConfiguredFeatures.LAKE_MOLTEN_STONE), List.of(
-                RarityFilter.onAverageOnceEvery(25),
-                InSquarePlacement.spread(),
-                HeightmapPlacement.onHeightmap(Heightmap.Types.WORLD_SURFACE_WG)
-        ));
-
 
         register(context, ModPlacedFeatures.SLIMY_TREE, configuredFeatureGetter.getOrThrow(ModConfiguredFeatures.SLIMY_TREE), VegetationPlacements.treePlacement(PlacementUtils.countExtra(1, 0.005f, 1), ModBlocks.SLIMY_SAPLING.get()));
     }
