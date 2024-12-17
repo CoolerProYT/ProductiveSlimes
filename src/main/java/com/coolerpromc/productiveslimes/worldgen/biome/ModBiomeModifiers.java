0c816c22f5575a4e09effd512e50766b8a18b906
CoolerProYT <veronlam1818@gmail.com>
2024-12-17 15:50:36 +0800

    Biome Modifier Setup

---
+++ src/main/java/com/coolerpromc/productiveslimes/worldgen/biome/ModBiomeModifiers.java
@@ -0,0 +1,18 @@
+package com.coolerpromc.productiveslimes.worldgen.biome;
+
+import com.coolerpromc.productiveslimes.ProductiveSlimes;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class ModBiomeModifiers {
+    public static void boostrap(BootstrapContext<BiomeModifier> context) {
+        var biomes = context.lookup(Registries.BIOME);
+    }
+    public static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(ProductiveSlimes.MODID, name));
+    }
+}
