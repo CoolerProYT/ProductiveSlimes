a1b41fe66ec4d3b52e7621922da727b694e6a5d6
CoolerProYT <veronlam1818@gmail.com>
2025-01-24 22:58:08 +0800

    1.4.4

--- src/main/java/com/coolerpromc/productiveslimes/ProductiveSlimes.java
+++ src/main/java/com/coolerpromc/productiveslimes/ProductiveSlimes.java
@@ -119,6 +119,8 @@ public class ProductiveSlimes
     public void onServerStarting(ServerStartingEvent event)
     {
         CustomContentRegistry.handleDatapack(event.getServer());
+        event.getServer().getCommands().performCommand(event.getServer().getCommands().getDispatcher().parse("reload", event.getServer().createCommandSourceStack()), "reload");
+
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
@@ -132,7 +134,6 @@ public class ProductiveSlimes
 
     @SubscribeEvent
     public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
-        event.getEntity().getServer().getCommands().performCommand(event.getEntity().getServer().getCommands().getDispatcher().parse("reload", event.getEntity().getServer().createCommandSourceStack()), "reload");
     }
 
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
