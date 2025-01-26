4aaa323514a21a5d7efdd3eb1b36a3fb56b52a57
CoolerProYT <veronlam1818@gmail.com>
2025-01-26 17:51:06 +0800

    1.4.5

--- src/main/java/com/coolerpromc/productiveslimes/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/coolerpromc/productiveslimes/datagen/ModBlockTagGenerator.java
@@ -32,12 +32,16 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.SLIMY_WOOD.get())
                 .add(ModBlocks.STRIPPED_SLIMY_LOG.get())
                 .add(ModBlocks.STRIPPED_SLIMY_WOOD.get());
+
         this.tag(BlockTags.PLANKS)
                 .add(ModBlocks.SLIMY_PLANKS.get());
+
         this.tag(BlockTags.SAPLINGS)
                 .add(ModBlocks.SLIMY_SAPLING.get());
+
         this.tag(BlockTags.LEAVES)
                 .add(ModBlocks.SLIMY_LEAVES.get());
+
         this.tag(BlockTags.LOGS)
                 .add(ModBlocks.SLIMY_LOG.get())
                 .add(ModBlocks.SLIMY_WOOD.get())
@@ -49,10 +53,19 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.SLIMY_WOOD.get())
                 .add(ModBlocks.STRIPPED_SLIMY_LOG.get())
                 .add(ModBlocks.STRIPPED_SLIMY_WOOD.get());
+
         this.tag(BlockTags.FENCES)
                 .add(ModBlocks.SLIMY_FENCE.get());
+
         this.tag(BlockTags.FENCE_GATES)
                 .add(ModBlocks.SLIMY_FENCE_GATE.get());
+
+        this.tag(BlockTags.WOODEN_DOORS)
+                .add(ModBlocks.SLIMY_DOOR.get());
+
+        this.tag(BlockTags.WOODEN_TRAPDOORS)
+                .add(ModBlocks.SLIMY_TRAPDOOR.get());
+
         this.tag(BlockTags.WALLS)
                 .add(ModBlocks.SLIMY_COBBLESTONE_WALL.get())
                 .add(ModBlocks.SLIMY_COBBLED_DEEPSLATE_WALL.get());
@@ -63,21 +76,40 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.ENERGY_GENERATOR.get())
                 .add(ModBlocks.DNA_EXTRACTOR.get())
                 .add(ModBlocks.DNA_SYNTHESIZER.get())
-                .add(ModBlocks.SLIME_SQUEEZER.get())
                 .add(ModBlocks.FLUID_TANK.get())
+                .add(ModBlocks.SLIME_SQUEEZER.get())
                 .add(ModBlocks.CABLE.get())
                 .add(ModBlocks.SLIME_NEST.get())
                 .add(ModBlocks.SLIMEBALL_COLLECTOR.get())
                 .add(ModBlocks.SLIMY_STONE.get())
                 .add(ModBlocks.SLIMY_DEEPSLATE.get())
                 .add(ModBlocks.SLIMY_COBBLESTONE.get())
-                .add(ModBlocks.SLIMY_COBBLED_DEEPSLATE.get());
+                .add(ModBlocks.SLIMY_COBBLED_DEEPSLATE.get())
+                .add(ModBlocks.SLIMY_COBBLESTONE_SLAB.get())
+                .add(ModBlocks.SLIMY_COBBLED_DEEPSLATE_SLAB.get())
+                .add(ModBlocks.SLIMY_COBBLESTONE_STAIRS.get())
+                .add(ModBlocks.SLIMY_COBBLED_DEEPSLATE_STAIRS.get())
+                .add(ModBlocks.SLIMY_COBBLESTONE_WALL.get())
+                .add(ModBlocks.SLIMY_COBBLED_DEEPSLATE_WALL.get())
+                .add(ModBlocks.SLIMY_STONE_STAIRS.get())
+                .add(ModBlocks.SLIMY_STONE_SLAB.get())
+                .add(ModBlocks.SLIMY_STONE_BUTTON.get())
+                .add(ModBlocks.SLIMY_STONE_PRESSURE_PLATE.get());
 
         this.tag(BlockTags.MINEABLE_WITH_AXE)
                 .add(ModBlocks.SLIMY_LOG.get())
                 .add(ModBlocks.SLIMY_WOOD.get())
                 .add(ModBlocks.STRIPPED_SLIMY_LOG.get())
-                .add(ModBlocks.STRIPPED_SLIMY_WOOD.get());
+                .add(ModBlocks.STRIPPED_SLIMY_WOOD.get())
+                .add(ModBlocks.SLIMY_PLANKS.get())
+                .add(ModBlocks.SLIMY_FENCE.get())
+                .add(ModBlocks.SLIMY_FENCE_GATE.get())
+                .add(ModBlocks.SLIMY_PRESSURE_PLATE.get())
+                .add(ModBlocks.SLIMY_BUTTON.get())
+                .add(ModBlocks.SLIMY_DOOR.get())
+                .add(ModBlocks.SLIMY_TRAPDOOR.get())
+                .add(ModBlocks.SLIMY_STAIRS.get())
+                .add(ModBlocks.SLIMY_SLAB.get());
 
         this.tag(BlockTags.MINEABLE_WITH_SHOVEL)
                 .add(ModBlocks.SLIMY_GRASS_BLOCK.get())
@@ -94,8 +126,5 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.CABLE.get())
                 .add(ModBlocks.SLIME_NEST.get())
                 .add(ModBlocks.SLIMEBALL_COLLECTOR.get());
-
-        this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL);
-//                .add(ModBlocks.ALUMINUM_BLOCK.get());
     }
 }
