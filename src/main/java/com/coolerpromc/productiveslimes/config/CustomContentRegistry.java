67426cffa63d3c54035786e802099846603b0d3e
CoolerProYT <veronlam1818@gmail.com>
2025-01-30 20:13:37 +0800

    v1.4.8 - New Guidebook Screen

--- src/main/java/com/coolerpromc/productiveslimes/config/CustomContentRegistry.java
+++ src/main/java/com/coolerpromc/productiveslimes/config/CustomContentRegistry.java
@@ -8,6 +8,8 @@ import com.coolerpromc.productiveslimes.entity.slime.BaseSlime;
 import com.coolerpromc.productiveslimes.entity.slime.Slime;
 import com.coolerpromc.productiveslimes.item.custom.DnaItem;
 import com.coolerpromc.productiveslimes.item.custom.SlimeballItem;
+import com.coolerpromc.productiveslimes.tier.ModTierLists;
+import com.coolerpromc.productiveslimes.tier.ModTiers;
 import com.coolerpromc.productiveslimes.util.InMemoryDataPack;
 import com.coolerpromc.productiveslimes.util.InMemoryResourcePack;
 import com.google.common.reflect.TypeToken;
@@ -205,6 +207,9 @@ public class CustomContentRegistry {
                     registerSlime(ENTITY_TYPES, variant);
                     registerSpawnEggItem(ITEMS, variant);
                     registerFluid(variant);
+
+                    ModTiers registerTier = new ModTiers(variant.getName(), variant.getColor(), variant.mapColorId, variant.cooldown, variant.growthItem, variant.solidingOutput, variant.solidingOutputCount, variant.synthesizingInputItem, variant.synthesizingInputDna1, variant.synthesizingInputDna2, (float) variant.dnaOutputChance);
+                    ModTierLists.addRegisteredTier(variant.getName(), registerTier);
                 }
 
                 LOGGER.info("Loaded " + loadedVariants.size() + " custom tiers");
--- src/main/java/com/coolerpromc/productiveslimes/screen/GuidebookMenu.java
+++ src/main/java/com/coolerpromc/productiveslimes/screen/GuidebookMenu.java
@@ -6,10 +6,14 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class GuidebookMenu extends AbstractContainerMenu {
+    public final Level level;
+
     public GuidebookMenu(int containerId, Inventory playerInventory) {
         super(ModMenuTypes.GUIDEBOOK_MENU.get(), containerId);
+        this.level = playerInventory.player.level();
     }
 
     public GuidebookMenu(int containerId, Inventory playerInventory, FriendlyByteBuf extraData) {
