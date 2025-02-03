2d5e2f7eec479240e4ad04bdb2114d4e71eb05a4
CoolerProYT <veronlam1818@gmail.com>
2025-02-03 20:56:29 +0800

    Localization Update

--- src/main/java/com/coolerpromc/productiveslimes/screen/SlimeballCollectorScreen.java
+++ src/main/java/com/coolerpromc/productiveslimes/screen/SlimeballCollectorScreen.java
@@ -11,11 +11,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-public class SlimeballCollectorScreen extends AbstractContainerScreen<SlimeballCollectorMenu>{
+
+public class SlimeballCollectorScreen extends AbstractContainerScreen<SlimeballCollectorMenu> {
     private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(ProductiveSlimes.MODID, "textures/gui/slimeball_collector_gui.png");
+
     public SlimeballCollectorScreen(SlimeballCollectorMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
     }
+
     @Override
     protected void init() {
         super.init();
@@ -24,10 +27,11 @@ public class SlimeballCollectorScreen extends AbstractContainerScreen<SlimeballC
         this.titleLabelY = 5;
         int x = (width - imageWidth) / 2;
         int y = (height - imageHeight) / 2;
-        this.addRenderableWidget(new Button.Builder(Component.literal("Toggle Area"), button -> {
+        this.addRenderableWidget(new Button.Builder(Component.translatable("gui.productiveslimes.toggle_area"), button -> {
             this.menu.blockEntity.setEnableOutline(this.menu.blockEntity.getData().get(0) == 0 ? 1 : 0);
         }).pos(x + 88, y + 65).size(80, 16).build());
     }
+
     @Override
     protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
@@ -36,6 +40,7 @@ public class SlimeballCollectorScreen extends AbstractContainerScreen<SlimeballC
         int y = (height - imageHeight) / 2;
         pGuiGraphics.blit(RenderType::guiTextured, TEXTURE, x, y, 0, 0, imageWidth, imageHeight, 256, 256);
     }
+
     @Override
     public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
         renderBackground(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
