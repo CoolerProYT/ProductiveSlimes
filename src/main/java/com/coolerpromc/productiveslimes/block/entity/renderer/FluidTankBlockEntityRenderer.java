41f733fb1b55ac6e944f90d6e6470ff579ff0c3c
CoolerProYT <veronlam1818@gmail.com>
2025-03-12 14:55:37 +0800

    Updated Fluid Tank item model

--- src/main/java/com/coolerpromc/productiveslimes/block/entity/renderer/FluidTankBlockEntityRenderer.java
+++ src/main/java/com/coolerpromc/productiveslimes/block/entity/renderer/FluidTankBlockEntityRenderer.java
@@ -1,8 +1,6 @@
 package com.coolerpromc.productiveslimes.block.entity.renderer;
 
 import com.coolerpromc.productiveslimes.block.entity.FluidTankBlockEntity;
-import com.coolerpromc.productiveslimes.block.entity.SolidingStationBlockEntity;
-import com.coolerpromc.productiveslimes.item.custom.BucketItem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
@@ -13,14 +11,8 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.neoforged.neoforge.fluids.FluidStack;
 
@@ -31,29 +23,21 @@ public class FluidTankBlockEntityRenderer implements BlockEntityRenderer<FluidTa
 
     @Override
     public void render(FluidTankBlockEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
-        ItemLike itemStack = pBlockEntity.getFluidStack().getFluid().getBucket();
         FluidStack fluidStack = pBlockEntity.getFluidStack();
-        int color = 0xFFFFFFFF;
+        renderFluid(pPoseStack, pBufferSource, pPackedLight, pPackedOverlay, fluidStack);
+    }
 
-        if (itemStack instanceof BucketItem bucketItem) {
-            color = bucketItem.getColor();
-        }
-
+    public static void renderFluid(PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int packedOverlay, FluidStack fluidStack){
         if (fluidStack.isEmpty()) return;
 
-        Level level = pBlockEntity.getLevel();
-        if (level == null) return;
-
-        BlockPos pos = pBlockEntity.getBlockPos();
-
-        IClientFluidTypeExtensions fluidTypeExtensions = fluidStack.isEmpty() ? IClientFluidTypeExtensions.of(Fluids.WATER.getFluidType()) : IClientFluidTypeExtensions.of(fluidStack.getFluid());
+        IClientFluidTypeExtensions fluidTypeExtensions = IClientFluidTypeExtensions.of(fluidStack.getFluid());
         ResourceLocation stillTexture = fluidTypeExtensions.getStillTexture(fluidStack);
-        if (stillTexture == null) return;
 
         FluidState state = fluidStack.getFluid().defaultFluidState();
 
+        int color = fluidTypeExtensions.getTintColor();
+
         TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(stillTexture);
-        int tintColor = color;
 
         float height = ((float) fluidStack.getAmount() / 50000) * 0.90f;
         if (fluidStack.getAmount() > 1000) {
@@ -62,23 +46,23 @@ public class FluidTankBlockEntityRenderer implements BlockEntityRenderer<FluidTa
 
         VertexConsumer builder = pBufferSource.getBuffer(ItemBlockRenderTypes.getRenderLayer(state));
 
-        drawQuad(builder, pPoseStack, 0.2f, height, 0.2f, 0.80f, height, 0.80f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, tintColor);
+        drawQuad(builder, pPoseStack, 0.2f, height, 0.2f, 0.80f, height, 0.80f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, color);
 
-        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.2f, 0.80f, height, 0.2f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, tintColor);
+        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.2f, 0.80f, height, 0.2f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, color);
         pPoseStack.pushPose();
         pPoseStack.mulPose(Axis.YP.rotationDegrees(180));
         pPoseStack.translate(-1f, 0, -1.6f);
-        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.80f, 0.80f, height, 0.80f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, tintColor);
+        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.80f, 0.80f, height, 0.80f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, color);
         pPoseStack.popPose();
         pPoseStack.pushPose();
         pPoseStack.mulPose(Axis.YP.rotationDegrees(90));
         pPoseStack.translate(-1f, 0, 0);
-        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.2f, 0.80f, height, 0.2f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, tintColor);
+        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.2f, 0.80f, height, 0.2f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, color);
         pPoseStack.popPose();
         pPoseStack.pushPose();
         pPoseStack.mulPose(Axis.YN.rotationDegrees(90));
         pPoseStack.translate(0, 0, -1f);
-        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.2f, 0.80f, height, 0.2f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, tintColor);
+        drawQuad(builder, pPoseStack, 0.2f, 0.05f, 0.2f, 0.80f, height, 0.2f, sprite.getU0(), sprite.getV0(), sprite.getU1(), sprite.getV1(), pPackedLight, color);
         pPoseStack.popPose();
     }
 
---
+++ src/main/java/com/coolerpromc/productiveslimes/util/FluidTankSpecialRenderer.java
@@ -0,0 +1,56 @@
+package com.coolerpromc.productiveslimes.util;
+
+import com.coolerpromc.productiveslimes.block.ModBlocks;
+import com.coolerpromc.productiveslimes.block.entity.renderer.FluidTankBlockEntityRenderer;
+import com.coolerpromc.productiveslimes.datacomponent.ModDataComponents;
+import com.coolerpromc.productiveslimes.handler.ImmutableFluidStack;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.special.SpecialModelRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.model.data.ModelData;
+import net.neoforged.neoforge.fluids.FluidStack;
+import org.jetbrains.annotations.Nullable;
+
+public record FluidTankSpecialRenderer() implements SpecialModelRenderer {
+    @Nullable
+    @Override
+    public ImmutableFluidStack extractArgument(ItemStack stack) {
+        return stack.get(ModDataComponents.FLUID_STACK);
+    }
+
+    @Override
+    public void render(@Nullable Object patterns, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, boolean hasFoilType) {
+        poseStack.pushPose();
+        BlockState blockState = ModBlocks.FLUID_TANK.get().defaultBlockState();
+        Minecraft.getInstance().getBlockRenderer().renderSingleBlock(blockState, poseStack, bufferSource, packedLight, packedOverlay, ModelData.EMPTY, RenderType.CUTOUT);
+        poseStack.popPose();
+
+        if (patterns instanceof ImmutableFluidStack immutableFluidStack){
+            FluidStack fluidStack = immutableFluidStack.fluidStack();
+            FluidTankBlockEntityRenderer.renderFluid(poseStack, bufferSource, packedLight, packedOverlay, fluidStack);
+        }
+    }
+
+    public record Unbaked(ResourceLocation texture) implements SpecialModelRenderer.Unbaked{
+        public static final MapCodec<Unbaked> MAP_CODEC = ResourceLocation.CODEC.fieldOf("texture").xmap(FluidTankSpecialRenderer.Unbaked::new, FluidTankSpecialRenderer.Unbaked::texture);
+
+        @Nullable
+        @Override
+        public SpecialModelRenderer<?> bake(EntityModelSet modelSet) {
+            return new FluidTankSpecialRenderer();
+        }
+
+        @Override
+        public MapCodec<? extends SpecialModelRenderer.Unbaked> type() {
+            return MAP_CODEC;
+        }
+    }
+}
