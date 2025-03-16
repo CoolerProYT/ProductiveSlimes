8099e891b60672288834d228bbf6e266008eb008
CoolerProYT <veronlam1818@gmail.com>
2025-03-16 16:58:29 +0800

    Fixed guidebook will give to player when join again if player death before

--- src/main/java/com/coolerpromc/productiveslimes/dataattachment/ModDataAttachments.java
+++ src/main/java/com/coolerpromc/productiveslimes/dataattachment/ModDataAttachments.java
@@ -12,7 +12,7 @@ import java.util.function.Supplier;
 public class ModDataAttachments {
     public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, ProductiveSlimes.MODID);
 
-    public static final Supplier<AttachmentType<Boolean>> IS_FIRST_TIME_LOGIN = ATTACHMENT_TYPES.register("is_first_time_login", () -> AttachmentType.builder(() -> true).serialize(Codec.BOOL).build());
+    public static final Supplier<AttachmentType<Boolean>> IS_FIRST_TIME_LOGIN = ATTACHMENT_TYPES.register("is_first_time_login", () -> AttachmentType.builder(() -> true).serialize(Codec.BOOL).copyOnDeath().build());
 
     public static void register(IEventBus eventBus){
         ATTACHMENT_TYPES.register(eventBus);
